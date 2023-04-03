cbfae31fbeda876939cdfe57533c4960e1371328
siddharthav45 <siddhartha.v21@iiits.in>
2023-04-03 23:13:46 +0530

    set up ts version

--- src/theme.ts
+++ src/theme.ts
@@ -1,8 +1,8 @@
-import {extendTheme, ThemeConfig} from '@chakra-ui/react'
+import { extendTheme, ThemeConfig } from '@chakra-ui/react'
 
-const config : ThemeConfig = {
-     initialColorMode:'dark'
+const config: ThemeConfig = {
+     initialColorMode: 'dark'
 }
-const theme = extendTheme({config});
+const theme = extendTheme({ config });
 
 export default theme;
