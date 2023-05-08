69173906d1912e8468a053fa0dbdc2cf39e9f905
siddharthav45 <siddhartha.v21@iiits.in>
2023-05-08 20:55:28 +0530

    clean up the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -5,6 +5,7 @@ import {
   Image,
   Spinner,
   Button,
+  Heading,
 } from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
@@ -25,27 +26,36 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
   return isLoading ? (
     <Spinner size="xl" />
   ) : (
-    <List>
-      {data.map((e) => (
-        <ListItem key={e.id} paddingY="5px">
-          <HStack>
-            <Image
-              src={getCroppedImageUrl(e.image_background)}
-              boxSize="32px"
-              borderRadius={8}
-            />
-            <Button
-              fontWeight={e.id === selectedGenre?.id ? "bold" : "normal"}
-              variant="link"
-              onClick={() => onSelectGenre(e)}
-              fontSize="lg"
-            >
-              {e.name.length < 16 ? e.name : e.name.slice(0, 16)}
-            </Button>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      <Heading fontSize={"2xl"} marginBottom="3">
+        Genres
+      </Heading>
+      <List>
+        {data.map((e) => (
+          <ListItem key={e.id} paddingY="5px">
+            <HStack>
+              <Image
+                src={getCroppedImageUrl(e.image_background)}
+                boxSize="32px"
+                borderRadius={8}
+                objectFit="cover"
+              />
+              <Button
+                fontWeight={e.id === selectedGenre?.id ? "bold" : "normal"}
+                variant="link"
+                whiteSpace={"normal"}
+                textAlign="left"
+                onClick={() => onSelectGenre(e)}
+                fontSize="lg"
+              >
+                {/* {e.name.length < 16 ? e.name : e.name.slice(0, 16)} */}
+                {e.name}
+              </Button>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 };
 
